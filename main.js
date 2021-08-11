b693be40fda69adaa9d0325e2f02ab4b0faffd3f
Saarah Bookwala <76689592+SaarahBookwala@users.noreply.github.com>
2021-08-11 16:18:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ leftWristX=0;
 leftWristY=0;
 rightWristX=0;
 rightWristY=0;
+scoreLeftWrist=0;
 song="";
 function preload(){
      song=loadSound('music.mp3');
@@ -19,13 +20,35 @@ function draw(){
      image(video,0,0,600,500);
      fill("#FF0000");
      stroke("#FF0000");
+     circle(rightWristX,rightWristY,20);
+     if(rightWristY>0 &&rightWristY<=100){
+          document.getElementById("speed").innerHTML="speed=0.5x";
+          song.rate(0.5);
+     }
+     else if(rightWristY>100 && rightWristY<=200){
+          document.getElementById("speed").innerHTML="speed=1x";
+          song.rate(1);
+     }
+     else if(rightWristY>200 && rightWristY<=300){
+          document.getElementById("speed").innerHTML="speed=1.5x";
+          song.rate(1.5);
+     }
+     else if(rightWristY>300 && rightWristY<=400){
+          document.getElementById("speed").innerHTML="speed=2x";
+          song.rate(2);
+     }
+     else if(rightWristY>400 && rightWristY<=500){
+          document.getElementById("speed").innerHTML="speed=2.5x";
+          song.rate(2.5);
+     }
+     if(scoreLeftWrist>0.2){
      circle(leftWristX,leftWristY,20);
      InNumberleftWristY=Number(leftWristY);
      remove_decimals=floor(InNumberleftWristY);
      volume=remove_decimals/500;
      document.getElementById("volume").innerHTML="Volume="+volume;
      song.setVolume(volume);
-     
+     }
 }
 function play(){
      song.play();
@@ -39,6 +62,8 @@ function modelLoaded(){
 function gotPoses(results){
 if(results.length>0){
      console.log(results);
+scoreLeftWrist=results[0].pose.keypoints[9].scoreLeftWrist;
+console.log("scoreLeftWrist=" + scoreLeftWrist);
  leftWristX=results[0].pose.leftWrist.x;
 leftWristY=results[0].pose.leftWrist.y;
 console.log(" leftWristX= " + leftWristX + " leftWristY= " + leftWristY );
